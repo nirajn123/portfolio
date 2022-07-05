865c8143c618a16cba61e88f37c4988bb6560930
nirajn123 <39927960+nirajn123@users.noreply.github.com>
2022-07-05 10:33:33 +0100

    Update fccsolved.js

--- fccsolved.js
+++ fccsolved.js
@@ -313,3 +313,16 @@ function steamrollArray(arr) {
 }
 
 console.log(steamrollArray([1, [2], [3, [[4]]]]));
+
+
+/*********
+* Return an English translated sentence of the passed binary string.
+* The binary string will be space separated.
+*
+***********/
+
+function binaryAgent(str) {
+  return str.split(" ").reduce((sum,item)=>sum+String.fromCharCode(parseInt(item,2)),""); 
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
